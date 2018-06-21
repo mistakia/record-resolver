15817d4e7bed13bc59459daf73d0f1147793a1b3
mistakia <1823355+mistakia@users.noreply.github.com>
2018-06-21 12:13:52 -0400

    fix: allow bundling via browserify

--- resolvers/index.js
+++ resolvers/index.js
@@ -1,16 +1,28 @@
 const fs = require('fs')
 
+let Resolvers = []
 let resolvers = []
-fs.readdirSync(__dirname).forEach((file) => {
-  if (file === 'index.js' || file === 'resolver.js') {
-    return
-  }
 
-  const Resolvers = require(`./${file}`).default
-  Resolvers.forEach((Resolver) => {
-    const resolver = new Resolver()
-    resolvers.push(resolver)
-  })
+const audiomackResolvers = require('./audiomack').default
+Resolvers = Resolvers.concat(audiomackResolvers)
+const bandcampResolvers = require('./bandcamp').default
+Resolvers = Resolvers.concat(bandcampResolvers)
+const eighttracksResolvers = require('./eighttracks').default
+Resolvers = Resolvers.concat(eighttracksResolvers)
+const hypemResolvers = require('./hypem').default
+Resolvers = Resolvers.concat(hypemResolvers)
+const mixcloudResolvers = require('./mixcloud').default
+Resolvers = Resolvers.concat(mixcloudResolvers)
+const soundcloudResolvers = require('./soundcloud').default
+Resolvers = Resolvers.concat(soundcloudResolvers)
+const youtubeResolvers = require('./youtube').default
+Resolvers = Resolvers.concat(youtubeResolvers)
+const vimeoResolvers = require('./vimeo').default
+Resolvers = Resolvers.concat(vimeoResolvers)
+
+Resolvers.forEach((Resolver) => {
+  const resolver = new Resolver()
+  resolvers.push(resolver)
 })
 
 module.exports = resolvers
